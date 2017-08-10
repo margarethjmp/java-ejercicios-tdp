88c7ca82b2824f06481e09113cf44f40cb86fc5b
Rhaynel Parra Aguiar <me@rhynl.io>
2017-08-09 22:44:22 -0300

    Ejercicio 01

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -1,7 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * EJERCICIO 01: Programa java que realice lo siguiente:
+ *  declarar una variable N de tipo int,
+ *  una variable A de tipo double y
+ *  una variable C de tipo char y asigna a cada una un valor.
+ *  A continuación muestra por pantalla: El valor de cada variable,
+ *  la suma de N + A, la diferencia de A - N, el valor numérico correspondiente
+ *  al carácter que contiene la variable C.
+ * SPOILER ALERT: posible solución aquí.
+    http://puntocomnoesunlenguaje.blogspot.com.es/2012/10/java-ejercicios-iniciales.html
  */
 package cac.java.tdp;
 
@@ -15,7 +21,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        
     }
     
 }
