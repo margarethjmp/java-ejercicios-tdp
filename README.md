bd0251e036e0daaa68bd7d844c06b261ef8431a4
Rhaynel Parra Aguiar <me@rhynl.io>
2017-08-09 23:06:01 -0300

    ejercicio06

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Ejercicios sobre tecnicas de programación (TDP) en Java
 
-Los ejercicios estan separados en diferentes ramas del repositorio, la idea es que cada estudiante descargue el repositorio, elija uno de los ejercicios y lo resuelva.
+## EJERCICIO 06: Programa java que realice lo siguiente:
+* declare una variable B de tipo entero y asígnale un valor.
+ * A continuación muestra un mensaje indicando si el valor de B es positivo o negativo.
+ * Consideraremos el 0 como positivo.
+ * Utiliza el operador condicional (? : ) dentro del println para resolverlo.
+## SPOILER ALERT
+
+Posible solución [aquí](http://puntocomnoesunlenguaje.blogspot.com.es/2012/10/java-ejercicios-iniciales-3.html).
--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * EJERCICIO 06: Programa Java que:
+ * declare una variable B de tipo entero y asígnale un valor.
+ * A continuación muestra un mensaje indicando si el valor de B es positivo o negativo.
+ * Consideraremos el 0 como positivo.
+ * Utiliza el operador condicional (? : ) dentro del println para resolverlo.
  */
 package cac.java.tdp;
 
@@ -15,7 +17,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        
     }
     
 }
