706460254dfd17936f25911de05af47cc9b5150e
Rhaynel Parra Aguiar <me@rhynl.io>
2017-08-09 23:23:47 -0300

    ejercicio07

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Ejercicios sobre tecnicas de programación (TDP) en Java
 
-Los ejercicios estan separados en diferentes ramas del repositorio, la idea es que cada estudiante descargue el repositorio, elija uno de los ejercicios y lo resuelva.
+## EJERCICIO 07: Programa java que realice lo siguiente:
+* declare una variable C de tipo entero y asígnale un valor.
+ * A continuación muestra un mensaje indicando si el valor de C es positivo o negativo,
+ * si es par o impar, si es múltiplo de 5,
+ * si es múltiplo de 10
+ * y si es mayor o menor que 100.
+ * Consideraremos el 0 como positivo.
+ * Utiliza el operador condicional ( ? : ) dentro del println para resolverlo.
+## SPOILER ALERT
+
+Posible solución [aquí](http://puntocomnoesunlenguaje.blogspot.com.es/2012/10/java-ejercicios-iniciales-3.html).
--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -1,7 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * EJERCICIO 07: Programa Java quE:
+ * declare una variable C de tipo entero y asígnale un valor.
+ * A continuación muestra un mensaje indicando si el valor de C es positivo o negativo,
+ * si es par o impar, si es múltiplo de 5,
+ * si es múltiplo de 10
+ * y si es mayor o menor que 100.
+ * Consideraremos el 0 como positivo.
+ * Utiliza el operador condicional ( ? : ) dentro del println para resolverlo.
  */
 package cac.java.tdp;
 
@@ -15,7 +20,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        
     }
     
 }
