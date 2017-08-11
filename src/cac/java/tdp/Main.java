0aa695833895567b87af063f76dc54541fcfb6d6
margareth <margarethjm@gmail.com>
2017-08-10 21:06:38 -0300

    Dia de la semana

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -15,7 +15,7 @@ package cac.java.tdp;
  *
  * @author Rhaynel Parra Aguiar <mail@rhynl.io>
  */
-
+import java.util.Scanner;
 
 public class Main {
 
@@ -24,27 +24,53 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        int N;
-        double A;
-        char C;
-        double Suma;
-        double Resta;
-        double J;
-        
-    
-        N=8;
-        A=3.14;
-        C='M';
-        J=(byte)C;
+        Scanner entrada= new Scanner (System.in);
+       int Dia;
        
-        Suma=N+A;
-        Resta=A-N;
-        
-        System.out.println("El valor de la suma es: " +Suma);
-        System.out.println("El valor de la resta es: " +Resta);
-        System.out.println("El valor numerico de C es: " +J);
-     
- 
+       System.out.println("Ingrese un numero del 1 al 7: ");
+       Dia= entrada.nextInt();
+       
+       if (Dia<1 && Dia>7){
+           System.out.println("Error. Ingrese un numero del 1 al 7");
+       }
+           else {
+                  if (Dia==1){
+                      System.out.println("El dia de la semana es Lunes");
+                                        } 
+                  else{
+                      if (Dia==2){
+                          System.out.println("El dia de la semana es Martes");
+                   
+                      }
+                      else {
+                        if (Dia==3){
+                        System.out.println("El dia de la semana es Miercoles");
+                      }
+                        else{
+                            if (Dia==4){
+                            System.out.println("El dia de la semana es Jueves");
+                            }
+                            else{
+                                if (Dia==5){
+                                System.out.println("El dia de la semana es Viernes");
+                                }
+                                else{
+                                    if (Dia==6){
+                                    System.out.println("El dia de la semana es Sabado");
+                                    }
+                                    else{
+                                    if (Dia==7){
+                                    System.out.println("El dia de la semana es Domingo");
+                                    }
+                                        }
+                                }
+                               }
+                      }
+                      }
+                  }
+                   }
+    }
+
     }
     
-}
+
