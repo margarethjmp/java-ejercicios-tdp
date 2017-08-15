6a58f7b1ab9c90009e4038a8684537d43b551745
margareth <margareth@gmail.com>
2017-08-15 20:24:35 -0300

    Calcular la hipotenusa de un triangulo

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -4,6 +4,7 @@
  * teorema de Pitágoras. 
  */
 package cac.java.tdp;
+import java.util.Scanner;
 
 /**
  *
@@ -15,6 +16,21 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        double Cateto1;
+        double Cateto2;
+        double Hipotenusa;
+        
+        Scanner entrada= new Scanner(System.in);
+          
+        System.out.println("Ingrese el valor del cateto 1: ");
+        Cateto1= entrada.nextDouble();
+        
+        System.out.println("Ingrese el valor del cateto 2:");
+        Cateto2= entrada.nextDouble();
+        
+        Hipotenusa=Math.sqrt(Math.pow(Cateto1, 2)+ Math.pow(Cateto2, 2));
+        
+        System.out.printf("La hipotenusa del triangulo es:%.2f\n",Hipotenusa);
         
     }
     
